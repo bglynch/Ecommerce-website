f7856329b85e4379c1566fc4ed7414027ddb6cb6
Brian Lynch <bglynch17@gmail.com>
2018-06-27 13:20:22 +0000

    added shopping cart functionality

--- cart/urls.py
+++ cart/urls.py
@@ -1,9 +1,10 @@
 # /cart/urls.py
 
 from django.urls import path
-from .views import get_cart,cart_add
+from .views import get_cart,cart_add, cart_remove
 
 urlpatterns = [
     path('', get_cart, name='get_cart'),
     path('add', cart_add, name='cart_add'),
+    path('remove', cart_remove, name='cart_remove'),
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,11 +1,30 @@
 # /cart/views.py
 
-from django.shortcuts import render, HttpResponse, get_object_or_404
+from django.shortcuts import render, redirect, HttpResponse, get_object_or_404
 from products.models import Product
+from decimal import Decimal
 
 # Create your views here.
 def get_cart(request):
-    return render(request, "cart/viewcart.html")
+    cart =  request.session.get('cart', {})
+    
+    cart_items = []
+    cart_total=0
+    items_total=0
+    for p in cart:
+        product = get_object_or_404(Product, pk = p)
+        
+        cart_item = {
+            'product':product, 
+            'quantity':cart[p],
+            'total':Decimal(product.price*cart[p]),
+        }
+        
+        cart_items.append(cart_item)
+        cart_total += Decimal(product.price*cart[p])
+        items_total += cart[p]
+    
+    return render(request, "cart/viewcart.html", {'cart_items': cart_items, 'cart_total':cart_total, 'items_total':items_total})
     
 def cart_add(request):
     # Get the product were adding
@@ -25,3 +44,14 @@ def cart_add(request):
     
     # Redirect to somewhere
     return HttpResponse(cart[id])
+    
+def cart_remove(request):
+    id = request.POST.get('product_id')
+    product = get_object_or_404(Product, pk=id)
+    cart = request.session.get('cart', {})
+    if cart.get(id) > 1:
+       cart[id] = cart.get(id, 0) + -1
+    else:
+        del cart[id]
+    request.session['cart'] = cart
+    return redirect('get_cart')
